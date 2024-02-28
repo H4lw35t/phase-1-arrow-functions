3f539240fbb25b7268fe48c5dab7a5e57e4695c4
Halwest Abubakir <halwest.east@gmail.com>
2024-02-28 17:08:01 +0300

    done

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = (a, b) => {
+    if (b === 0) {
+      return "can't divide by zero";
+    }
+    return a / b;
+  };
+  
+ 
+  const square = (x) => {
+    return x * x;
+  };
+  
+
+  const add = (a, b) => {
+    return a + b;
+  };
